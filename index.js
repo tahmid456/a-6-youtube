dff225ebf9d0b1445eec726c6768a5195d112f6c
unknown <imrojtahmid@gmail.com>
2023-09-04 18:47:25 +0600

    header is finished

--- index.js
+++ index.js
@@ -24,3 +24,48 @@ questionAnswer.appendChild(p);
 
 
 }
+const loadPhone = async () =>{
+
+const res = await fetch(" https://openapi.programming-hero.com/api/videos/category/1000");
+const data = await res.json();
+const phones = data.data;
+// console.log(phones);
+displayPhones(phones);
+
+
+
+
+}
+
+
+
+const displayPhones = phones =>{
+
+    // console.log(phones);
+    // step 1
+    const phoneContainer = document.getElementById("phone-container");
+    phones.forEach(phone =>{
+
+        console.log(phone);
+        // step2
+        const phoneCard = document.createElement("div");
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        // step 3
+        phoneCard.innerHTML=`<figure><img src="${phone.thumbnail}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.title}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>`;
+
+
+
+        // step 4 append child;
+        phoneContainer.appendChild(phoneCard);
+    })
+
+    
+}
+loadPhone();
